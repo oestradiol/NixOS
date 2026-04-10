8642d8f4c2b651b650636dffeb10b37bd8fce802
Elaina <48662592+oestradiol@users.noreply.github.com>
2026-04-09 22:24:19 -0300

    feat: add myOS.controllers.enable knob for Bluetooth/Xbox controllers
    - options.nix: Added controllers.enable option (default: false)
    - gaming.nix: Wired up controller config with lib.mkIf conditional
      - xpadneo, bluetooth, udev rules, blueman all conditional on knob
    - Preserves existing gaming config (Steam, Gamescope, Gamemode)
    
    To enable: myOS.security.controllers.enable = true;

--- modules/desktop/gaming.nix
+++ modules/desktop/gaming.nix
@@ -1,66 +1,98 @@
 { config, lib, pkgs, ... }: {
-  imports = [ ./vr.nix ]; #./controllers.nix
+  imports = [ ./vr.nix ];
 
-  # Steam
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = false; # For Steam Link
-
-    package = pkgs.steam.override {
-      extraProfile = ''
-        unset TZ
-        export PRESSURE_VESSEL_IMPORT_OPENXR_1_RUNTIMES=1
-      '';
-
-      # Expose WiVRn IPC socket to Steam's pressure-vessel sandbox.
-      extraEnv = {
-        PRESSURE_VESSEL_FILESYSTEMS_RW = "$XDG_RUNTIME_DIR/wivrn/comp_ipc";
+  config = {
+    # Controller support (Bluetooth/Xbox) - disabled by default, enable with myOS.controllers.enable
+    hardware.xpadneo.enable = lib.mkIf config.myOS.security.controllers.enable true;
+    
+    hardware.bluetooth = lib.mkIf config.myOS.security.controllers.enable {
+      enable = true;
+      powerOnBoot = true;
+      settings = {
+        General = {
+          Experimental = true;
+          MultiProfile = "multiple";
+          FastConnectable = true;
+          KernelExperimental = "6fbaf188-05e0-496a-9885-d6ddfdb4e03e,330859bc-7506-492d-9370-9a6f0614037f";
+        };
       };
     };
 
-    # Proton-GE for better game compatibility + umu-launcher
-    extraCompatPackages = with pkgs; [
-      #proton-ge-bin
+    boot.extraModprobeConfig = lib.mkIf config.myOS.security.controllers.enable ''
+      options bluetooth disable_ertm=1
+    '';
+
+    services.udev.packages = lib.mkIf config.myOS.security.controllers.enable [ pkgs.game-devices-udev-rules ];
+    
+    services.udev.extraRules = lib.mkIf config.myOS.security.controllers.enable ''
+      SUBSYSTEMS=="usb", TAG+="uaccess"
+      KERNEL=="hidraw*", TAG+="uaccess"
+      KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="028e", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="0719", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="02ea", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="0b12", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="05c4", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="09cc", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ce6", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="2dc8", ATTRS{idProduct}=="310b", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="2dc8", ATTRS{idProduct}=="6012", TAG+="uaccess"
+    '';
+
+    services.blueman.enable = lib.mkIf config.myOS.security.controllers.enable true;
+
+    # Steam
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = false;
+      package = pkgs.steam.override {
+        extraProfile = ''
+          unset TZ
+          export PRESSURE_VESSEL_IMPORT_OPENXR_1_RUNTIMES=1
+        '';
+        extraEnv = {
+          PRESSURE_VESSEL_FILESYSTEMS_RW = "$XDG_RUNTIME_DIR/wivrn/comp_ipc";
+        };
+      };
+      extraCompatPackages = with pkgs; [
+        #proton-ge-bin
+      ];
+    };
+
+    # Graphics
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    hardware.steam-hardware.enable = true;
+
+    # Gamescope
+    programs.gamescope = {
+      enable = true;
+      capSysNice = true;
+    };
+    programs.steam.gamescopeSession = {
+      enable = true;
+    };
+
+    # Feral Gamemode
+    programs.gamemode.enable = true;
+
+    # Environment
+    environment.systemPackages = with pkgs; [
+      #mangohud
+      #protonup-qt
+      #protontricks
     ];
-  };
+    environment.sessionVariables = {
+      PROTON_USE_NTSYNC = "1";
+      ENABLE_GAMESCOPE_WSI = "1";
+    };
 
-  # Graphics
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-  hardware.steam-hardware.enable = true;
-
-  # Gamescope
-  programs.gamescope = {
-    enable = true;
-    # Creates /run/wrappers/bin/gamescope with cap_sys_nice capability.
-    # Required for Steam to function inside gamescope.
-    # Also sets security.wrappers.bwrap with setuid.
-    capSysNice = true;
-  };
-  programs.steam.gamescopeSession = {
-    enable = true;
-  };
-
-  # Feral Gamemode
-  programs.gamemode.enable = true;
-
-  # Environment
-  environment.systemPackages = with pkgs; [
-    #mangohud     # Vulkan + OpenGL system monitoring overlay
-    #protonup-qt   # GUI for managing Proton-GE and other custom Proton versions
-    #protontricks  # Run winetricks commands inside Proton prefixes
-  ];
-  environment.sessionVariables = {
-    PROTON_USE_NTSYNC = "1";
-    ENABLE_GAMESCOPE_WSI = "1";
-    #STEAM_MULTIPLE_XWAYLANDS = "1"; # Great but makes VRChat not open :(
-  };
-
-  # xdg portal for Flatpak and Wayland app integration
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    # xdg portal
+    xdg.portal = {
+      enable = true;
+      extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    };
   };
 }
